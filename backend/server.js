720b515ccec5b92934a382ca9671ba17e09fe235
Aron Limos <157419134+AronLimos@users.noreply.github.com>
2025-03-07 20:45:00 -0700

    Added Frontend(React) & Backend Folder

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,10 @@ connectDB();
 const app = express();
 app.use(express.json());
 
-app.use('/api/users', require('./routes/userRoutes'));
-app.use('/api/shops', require('./routes/shopRoutes'));
-app.use('/api/bookings', require('./routes/bookingRoutes'));
-app.use('/api/reviews', require('./routes/reviewRoutes'));
+app.use('/api/users', require('../routes/userRoutes'));
+app.use('/api/shops', require('../routes/shopRoutes'));
+app.use('/api/bookings', require('../routes/bookingRoutes'));
+app.use('/api/reviews', require('../routes/reviewRoutes'));
 
 app.get('/', (req, res) => res.send('API Running...'));
 
